b624c2dc431806bb13257db882e7d44dec4132ff
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-11-29 14:37:19 +0700

    Added v4-section8 with eureka server

---
+++ v4-section8/eurekaserver/src/test/java/com/arbi/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbi.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
